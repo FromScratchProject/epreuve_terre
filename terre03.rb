d1d180626449140526345aae2a36cf532e7337bb
Rémi <92462525+FromScratchProject@users.noreply.github.com>
2022-01-29 15:35:19 +0100

    Add files via upload

---
+++ terre03.rb
@@ -0,0 +1,10 @@
+alpha = "abcdefghijklmnopqrstuvwxyz"
+
+# Je souhaite afficher l'alphabet à partir de la lettre K
+for arg in ('a'...'z')
+	puts "#{alpha[10...26]}"
+	break
+end
+
+# Retour à la ligne
+	puts "\n"
